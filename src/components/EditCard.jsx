410f75ae1caa8813a985ef2b31742799c5de7933
Luis <luiswoohoo@gmail.com>
2023-04-02 23:48:54 -0500

    Minor edit

--- src/components/EditCard.jsx
+++ src/components/EditCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 function EditCard({ front, back, updateCardInfo }) {
     return (
